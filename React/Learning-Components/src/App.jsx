01bd2ec832e0dbd2e1f65343fb906f558ce417c0
Ilya <iliadorvar@gmail.com>
2025-03-06 12:43:48 +0330

    Learning React Components

---
+++ React/Learning-Components/src/App.jsx
@@ -0,0 +1,16 @@
+import Button from "./components/Button";
+import Heading from "./components/Heading";
+import Footer from "./components/Footer";
+import "./public/css/App.css";
+
+function App() {
+  return (
+    <>
+      <Heading />
+      <Button />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
---
+++ React/Learning-Components/src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import "../public/css/Footer.css";
+
+function Footer() {
+  return (
+    <>
+      <p>I'm Learning React Components 🙂</p>
+    </>
+  );
+}
+
+export default Footer;
